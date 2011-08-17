7d057f1819578ddffa9686a14c8b1004f5115018
Andrew Kornilov <frutik@gmail.com>
2011-08-17 20:37:18 +0300

    fixed wrong configured menu

--- simple_vxml_menu/menu/views.py
+++ simple_vxml_menu/menu/views.py
@@ -10,4 +10,7 @@ def vxml_file(request, slug):
 
     menu_items = MenuItem.objects.filter(menu=menu)
 
+    if not menu_items:
+        raise Http404()
+
     return render_to_response('menu/vxml.xml', {'menu': menu, 'menu_items': menu_items}, mimetype = 'text/xml')
