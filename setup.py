f51b0f3d3bc4137f8b427ebb67b8bbb7572e2695
Andrew Kornilov <frutik@gmail.com>
2011-08-18 09:55:12 +0300

    added pypi setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools import setup
+setup(
+    name = "django-simple-vxml-menu",
+    version = "0.1",
+    packages = "django-simple-vxml-menu",
+    author = "Andrew Kornilov",
+    author_email = "frutik@gmail.com",
+    description = "A package to help manage vxml menus in Django",
+    url = "http://code.google.com/p/django-testmaker/",
+    include_package_data = True
+)
